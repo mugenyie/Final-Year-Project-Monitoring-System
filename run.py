5b2fb954a7ed80724d541ea5ea936f76aa965fac
Emmanuel Columbus Mugenyi <ecmugenyi@gmail.com>
2019-04-29 13:32:33 +0300

    Edit run.py

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import os
 from src.app import create_app
 
 if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
+  env_name = os.getenv('FLASK_ENV') or 'development'
   app = create_app(env_name)
   app.run()
