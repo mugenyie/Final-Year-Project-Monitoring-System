fb98eb01808cd6b7cf37383a927959a1541dc13e
Emmanuel Columbus Mugenyi <ecmugenyi@gmail.com>
2019-04-29 15:18:58 +0300

    Add PasswordService

---
+++ src/services/PasswordService.py
@@ -0,0 +1,9 @@
+class GeneratePassword:
+    def __init__(self, length):
+        self.length = length
+
+    def generate_pin(self):
+        return ""
+        
+    def generate_password(self):
+        return ""
