1e3396b6717687444cf1614c9fd6e37df4adc88d
Emmanuel Columbus Mugenyi <ecmugenyi@gmail.com>
2019-04-29 15:39:29 +0300

    Implement password service

--- src/services/PasswordService.py
+++ src/services/PasswordService.py
@@ -2,8 +2,10 @@ import random, string
 
 class GeneratePassword:
     """Generate random password or pin"""
+    __password_choice = string.digits + string.ascii_letters + string.punctuation
+    
     def generate_pin(self, length=6):
-        return "".join(random.choice(string.digits) for i in range(length))
-        
+        return "".join(random.choice(string.digits) for i in range(length)) 
+
     def generate_password(self, length=6):
-        return "".join(random.choice(string.ascii_letters) for i in range(length))
+        return "".join(random.choice(__password_choice) for i in range(length))
