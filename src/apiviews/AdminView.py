de312c709b46deed2e48018e6ab37b1b36a4028b
Emmanuel Columbus Mugenyi <ecmugenyi@gmail.com>
2019-05-12 02:21:08 +0300

    change 1

--- src/apiviews/AdminView.py
+++ src/apiviews/AdminView.py
@@ -1,6 +1,6 @@
 #/src/views/AdminView
 
-from flask import request, json, Response, Blueprint, g
+from flask import request, json, Response, Blueprint, g, jsonify
 from ..models.AdminModel import AdminModel, AdminSchema
 from ..services.AuthorizationService import Auth
 from ..apiviews import custom_response
@@ -109,4 +109,4 @@ def login():
     return custom_response({'error': 'invalid credentials'}, 400)
   user_data = admin_schema.dump(user).data
   token = Auth.generate_token(user_data.get('id'), user_data.get('user_role_value'))
-  return custom_response(user_data, token, 200)
+  return jsonify(api_toke=token, data=user_data),200 
