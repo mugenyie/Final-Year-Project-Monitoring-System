6a8f341e42036929b065643eb5d3dc5dc0c9f29f
Emmanuel Columbus Mugenyi <ecmugenyi@gmail.com>
2019-05-12 02:04:00 +0300

    change 1

--- src/apiviews/__init__.py
+++ src/apiviews/__init__.py
@@ -1,5 +1,11 @@
 from flask import jsonify
 
+def custom_response(data,status_code):
+    """
+    Custom Response Function
+    """
+    return jsonify(data), status_code
+
 def custom_response(data,api_token, status_code):
     """
     Custom Response Function
