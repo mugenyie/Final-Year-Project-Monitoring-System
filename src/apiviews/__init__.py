1d42c50a1eac5341da6fb5fdf73b5a500ce31514
Emmanuel Columbus Mugenyi <ecmugenyi@gmail.com>
2019-05-12 01:55:39 +0300

    Change custom reponse on API

--- src/apiviews/__init__.py
+++ src/apiviews/__init__.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 
-def custom_response(data, status_code):
+def custom_response(data,api_token, status_code):
     """
     Custom Response Function
     """
-    return jsonify(data), status_code
+    return jsonify(data=data,api_token=api_token), status_code
