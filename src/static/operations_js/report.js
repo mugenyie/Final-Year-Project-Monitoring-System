82d1c792098cb0851b2af57d539543d8bb97d8e7
Emmanuel Columbus Mugenyi <ecmugenyi@gmail.com>
2019-06-05 11:54:02 +0300

    Add chart.js

--- src/static/operations_js/report.js
+++ src/static/operations_js/report.js
@@ -1,42 +1,80 @@
-// var GraphData = new google.visualization.DataTable();
-// GraphData.addColumn('number', 'Week'); 
+function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
 
-// function GenerateReport(group_id){
-//     GetData(baseurl+`projectlog/${group_id}/group`)
-//     .then(data => {
-//         console.log(JSON.stringify(data));
-//         data.forEach(element => {
+ async function GenerateNames(group_id){
+    var config = new Object();
+    var chartData = {};
+    var x_list = new Array();
+
+    var data = GetData(baseurl+'student/group/'+group_id+'/members')
+        .then(groupData => {
+    
+            groupData.forEach(element => {
+                let color = random_rgba();
+                let dataset = {
+                    label: element.name,
+                    backgroundColor: color,
+                    borderColor: color,
+                    data: [
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100)
+                    ],
+                    fill: false
+                }
+                x_list.push(dataset);
+            });
             
-//         });
-//         document.getElementById('group_log').innerHTML = group_log;
-//     })
-//     .catch(error => console.error(error));
-// }
-// var x_list = [];
-// function GenerateNames(group_id){
-//     var resp = GetData();
-//     return resp;
-// }
+                chartData['type'] = 'line';
+                chartData['data'] = {
+                    labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8'],
+                    datasets: x_list
+                };
+                chartData['options'] = {
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: 'Student Group Performance. (Ploted using chart.js)'
+                    },
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Week'
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Performance (%)'
+                            }
+                        }]
+                    }
+                }
 
-!async function GenerateNames(group_id){
-    let data = await GetData(baseurl+'student/group/'+group_id+'/members')
-        .then(data => {
-            return data;
+            config = JSON.stringify(chartData);
+            return config;
         })
         .catch(error => {
             console.error(error);
         });
-    
-    console.log(data);
-    var names = GetNames(data);
-    console.log(names);
-    }();
-
-function GetNames(student_data){
-    let student_list = new Array();
-    student_data.forEach(element => {
-        student_list.push(element.name);
-        x = element.name;
-    });
-    return student_list;
-}
+        
+        return await data;
+    };
